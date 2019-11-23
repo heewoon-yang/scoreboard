cc70637b906cbbb77276e8342cf84a4d7c0f2c98
heewoon-yang <heewoon0@gmail.co.kr>
2019-11-23 18:07:07 +0900

    gitignore commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/Header";
+import {Player} from "./components/Player";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    players: [
+      { id: 1, name: 'LDK', score:10 },
+      { id: 2, name: 'PARK', score:20 },
+      { id: 3, name: 'KIM', score:30 },
+      { id: 4, name: 'HONG', score:40 },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header title="My Scoreboard" totalPlayers={11}/>
+        {
+          this.state.players.map((player) => (
+            <Player
+              key={player.id}
+              name={player.name}
+              id={player.id}
+              score = {player.score}
+              changeScore = {this.handleChangeScore}
+              removePlayer={this.handleRemovePlayer}
+            />
+          ))
+        }
+      </div>
+    )
+  }
+  handleRemovePlayer = (id) => {
+    console.log('removePlayer: ', id);
+    const players = this.state.players.filter(player => player.id !== id)
+    // this.state.players = players;
+    console.log(players)
+    this.setState(prevState => {
+      const players = prevState.players.filter(player => player.id !== id)
+      // Immutable 함수 = 새로운 배열을 리턴 = shallow comparison
+      // 키와 변수가 같을 경우 한쪽을 생략: shorthand property
+      return { players }
+    })
+  }
+
+  handleChangeScore = (id, delta) => {
+    console.log('handleChangeScore',id,' ',delta);
+  }
+
 }
 
 export default App;
