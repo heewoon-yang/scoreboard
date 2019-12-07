03a7e3f2fb3a46a5660ef23da47d0368dd36ef27
ohhhhhjy <ohhhhhjy@gmail.com>
2019-12-07 17:31:17 +0900

    gitignore commit

--- src/page/product/SearchBar.js
+++ src/page/product/SearchBar.js
@@ -3,8 +3,8 @@ import React from 'react';
 export const SearchBar = (props) => {
   return (
     <div>
-      <input placeholder="Search..." value={props.keyword} onChange={(e) => props.setKeyword(e.target.value)}></input><br />
-      <input type="checkbox" checked={props.stockChecked} onChange={(e) => props.setStockChecked(e.target.checked)}></input>Only show products in stock
+      <input placeholder="Search..." value={props.keyword} onChange={(e) => props.setKeyword(e.target.value)}/><br />
+      <input type="checkbox" checked={props.stockChecked} onChange={(e) => props.setStockChecked(e.target.checked)}/>Only show products in stock
     </div>
   )
 }
