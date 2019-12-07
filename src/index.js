c9f6cba483e53ca53259bae35b0ab14727f4dcf9
ohhhhhjy <ohhhhhjy@gmail.com>
2019-12-07 18:24:20 +0900

    gitignore commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {Provider} from "react-redux";
 import {store} from "./redux/store";
 import {FilterableProductTable} from "./page/product/FilterableProductTable";
 
-ReactDOM.render(<Provider store={store}><FilterableProductTable /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
